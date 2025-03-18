1c26dacd8b7c0d142f831460d8788fdc5529257f
Matei Hriscu <matei.hriscu@proton.me>
2025-03-18 18:31:16 +0200

    Refactor attacks
    Signed-off-by: Matei Hriscu <matei.hriscu@proton.me>

--- attacks.hpp
+++ attacks.hpp
@@ -3,68 +3,6 @@
 #include "magic.hpp"
 #include "types.hpp"
 
-template <Piece>
-inline constexpr enum_array<Square, Bitboard, NUM_SQUARES> pseudoattacks{};
-
-template <>
-inline constexpr enum_array<Square, Bitboard, NUM_SQUARES>
-    pseudoattacks<Piece::KNIGHT> = []() {
-      enum_array<Square, Bitboard, NUM_SQUARES> moves;
-
-      for (Square square : ALL_SQUARES) {
-        Bitboard bb = get_bit(square);
-
-        moves[square] = ((bb & ~(files[0] | ranks[6] | ranks[7])) << 15) |
-                        ((bb & ~(files[7] | ranks[6] | ranks[7])) << 17) |
-                        ((bb & ~(files[7] | ranks[0] | ranks[1])) >> 15) |
-                        ((bb & ~(files[0] | ranks[0] | ranks[1])) >> 17) |
-                        ((bb & ~(ranks[7] | files[0] | files[1])) << 6) |
-                        ((bb & ~(ranks[0] | files[0] | files[1])) >> 10) |
-                        ((bb & ~(ranks[0] | files[6] | files[7])) >> 6) |
-                        ((bb & ~(ranks[7] | files[6] | files[7])) << 10);
-      }
-
-      return moves;
-    }();
-
-template <>
-inline constexpr enum_array<Square, Bitboard, NUM_SQUARES>
-    pseudoattacks<Piece::KING> = []() {
-      enum_array<Square, Bitboard, NUM_SQUARES> moves;
-
-      for (Square square : ALL_SQUARES) {
-        Bitboard bb = get_bit(square);
-
-        moves[square] = ((bb & ~(ranks[0] | files[0])) >> 9) |
-                        ((bb & ~(ranks[0] | files[7])) >> 7) |
-                        ((bb & ~(ranks[7] | files[0])) << 7) |
-                        ((bb & ~(ranks[7] | files[7])) << 9) |
-                        ((bb & ~files[0]) >> 1) | ((bb & ~files[7]) << 1) |
-                        ((bb & ~ranks[0]) >> 8) | ((bb & ~ranks[7]) << 8);
-      }
-
-      return moves;
-    }();
-
-inline constexpr enum_array<Side, enum_array<Square, Bitboard, NUM_SQUARES>,
-                            NUM_SIDES>
-    pawn_moves = []() {
-      enum_array<Side, enum_array<Square, Bitboard, NUM_SQUARES>, NUM_SIDES>
-          moves = {};
-
-      for (Square square : ALL_SQUARES) {
-        Bitboard bb = get_bit(square);
-
-        moves[Side::WHITE][square] =
-            ((bb & ~(ranks[0] | ranks[7])) << 8) | ((bb & ranks[1]) << 16);
-
-        moves[Side::BLACK][square] =
-            ((bb & ~(ranks[0] | ranks[7])) >> 8) | ((bb & ranks[6]) >> 16);
-      }
-
-      return moves;
-    }();
-
 inline constexpr enum_array<Side, enum_array<Square, Bitboard, NUM_SQUARES>,
                             NUM_SIDES>
     pawn_attacks = []() {
@@ -88,13 +26,51 @@ template <Piece P>
 constexpr Bitboard attacks_bb(Square square, Bitboard occupied);
 
 template <>
-constexpr Bitboard attacks_bb<Piece::KING>(Square square, Bitboard) {
-  return pseudoattacks<Piece::KING>[square];
+constexpr Bitboard attacks_bb<Piece::KNIGHT>(Square square, Bitboard) {
+  static constexpr enum_array<Square, Bitboard, NUM_SQUARES> pseudoattacks =
+      []() {
+        enum_array<Square, Bitboard, NUM_SQUARES> moves;
+
+        for (Square square : ALL_SQUARES) {
+          Bitboard bb = get_bit(square);
+
+          moves[square] = ((bb & ~(files[0] | ranks[6] | ranks[7])) << 15) |
+                          ((bb & ~(files[7] | ranks[6] | ranks[7])) << 17) |
+                          ((bb & ~(files[7] | ranks[0] | ranks[1])) >> 15) |
+                          ((bb & ~(files[0] | ranks[0] | ranks[1])) >> 17) |
+                          ((bb & ~(ranks[7] | files[0] | files[1])) << 6) |
+                          ((bb & ~(ranks[0] | files[0] | files[1])) >> 10) |
+                          ((bb & ~(ranks[0] | files[6] | files[7])) >> 6) |
+                          ((bb & ~(ranks[7] | files[6] | files[7])) << 10);
+        }
+
+        return moves;
+      }();
+
+  return pseudoattacks[square];
 }
 
 template <>
-constexpr Bitboard attacks_bb<Piece::KNIGHT>(Square square, Bitboard) {
-  return pseudoattacks<Piece::KNIGHT>[square];
+constexpr Bitboard attacks_bb<Piece::KING>(Square square, Bitboard) {
+  static constexpr enum_array<Square, Bitboard, NUM_SQUARES> pseudoattacks =
+      []() {
+        enum_array<Square, Bitboard, NUM_SQUARES> moves;
+
+        for (Square square : ALL_SQUARES) {
+          Bitboard bb = get_bit(square);
+
+          moves[square] = ((bb & ~(ranks[0] | files[0])) >> 9) |
+                          ((bb & ~(ranks[0] | files[7])) >> 7) |
+                          ((bb & ~(ranks[7] | files[0])) << 7) |
+                          ((bb & ~(ranks[7] | files[7])) << 9) |
+                          ((bb & ~files[0]) >> 1) | ((bb & ~files[7]) << 1) |
+                          ((bb & ~ranks[0]) >> 8) | ((bb & ~ranks[7]) << 8);
+        }
+
+        return moves;
+      }();
+
+  return pseudoattacks[square];
 }
 
 template <>
