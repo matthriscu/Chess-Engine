3276e6351ffccefdbc853287b20837c33ec67b00
Matei Hriscu <matei.hriscu@proton.me>
2025-03-23 16:06:08 +0200

    Patch Q search
    Signed-off-by: Matei Hriscu <matei.hriscu@proton.me>

--- searcher.hpp
+++ searcher.hpp
@@ -9,17 +9,18 @@ class Searcher {
   std::chrono::steady_clock::time_point deadline;
   Move best_move_iter;
 
-  bool is_time_up() { return std::chrono::steady_clock::now() >= deadline; }
-
-  constexpr int quiesce(const Board &board, int ply, int alpha, int beta) {
+  bool is_time_up() {
     if (++nodes_searched == 1024) {
-      if (is_time_up()) {
-        timed_out = true;
-        return alpha;
-      }
-
+      timed_out = std::chrono::steady_clock::now() >= deadline;
       nodes_searched = 0;
     }
+
+    return timed_out;
+  }
+
+  constexpr int quiesce(const Board &board, int ply, int alpha, int beta) {
+    if (is_time_up())
+      return 0;
 
     if (board.is_check())
       return pvs(board, 1, ply, alpha, beta);
@@ -33,46 +34,47 @@ class Searcher {
       alpha = stand_pat;
 
     bool found_legal_move = false;
+    int best_value = -INF;
 
-    for (Move move :
-         std::views::filter(board.pseudolegal_moves(), &Move::is_capture)) {
+    MoveList moves = board.pseudolegal_moves();
+    std::ranges::sort(moves, std::bind_front(&Board::move_comparator, &board));
+
+    for (Move move : std::views::filter(moves, &Move::is_capture)) {
       Board copy = board;
       copy.make_move(move);
 
       if (copy.is_legal()) {
         found_legal_move = true;
 
-        int value = -quiesce(copy, -beta, -alpha, ply + 1);
+        int value = -quiesce(copy, ply + 1, -beta, -alpha);
+
+        if (timed_out)
+          return 0;
 
         if (value >= beta)
           return value;
 
-        if (value > alpha)
-          alpha = value;
+        best_value = std::max(best_value, value);
+        alpha = std::max(alpha, value);
       }
     }
 
-    return found_legal_move ? alpha : 0;
+    return found_legal_move ? best_value : 0;
   }
 
   constexpr int pvs(const Board &board, int depth, int ply = 0,
                     int alpha = -INF, int beta = INF) {
-    if (++nodes_searched == 1024) {
-      if (is_time_up()) {
-        timed_out = true;
-        return alpha;
-      }
-
-      nodes_searched = 0;
-    }
+    if (is_time_up())
+      return 0;
 
     if (depth == 0)
-      return board.evaluation();
+      return quiesce(board, ply, alpha, beta);
 
     MoveList moves = board.pseudolegal_moves();
     std::ranges::sort(moves, std::bind_front(&Board::move_comparator, &board));
 
     bool first_move = true, found_legal_move = false;
+    int best_value = -INF;
 
     for (Move move : moves) {
       Board copy = board;
@@ -93,8 +95,11 @@ class Searcher {
             value = -pvs(copy, depth - 1, ply + 1, -beta, -alpha);
         }
 
-        if (timed_out || value >= beta)
-          return beta;
+        if (timed_out)
+          return 0;
+
+        if (value > best_value)
+          best_value = value;
 
         if (value > alpha) {
           alpha = value;
@@ -102,10 +107,13 @@ class Searcher {
           if (ply == 0)
             best_move_iter = move;
         }
+
+        if (alpha >= beta)
+          break;
       }
     }
 
-    return found_legal_move ? alpha : board.is_check() ? ply - INF : 0;
+    return found_legal_move ? best_value : board.is_check() ? ply - INF : 0;
   }
 
 public:
