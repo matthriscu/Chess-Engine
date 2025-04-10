51ec85cc9b42a4959408bfb5277793bfab95e86f
Matei Hriscu <matei.hriscu@proton.me>
2025-04-10 12:42:50 +0300

    Update sprt script to compare git branches
    Signed-off-by: Matei Hriscu <matei.hriscu@proton.me>

--- sprt.sh
+++ sprt.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # This depends on running in the dev branch, with the main branch as a worktree in `main`
 
 current=$(git symbolic-ref --short HEAD)
@@ -11,6 +12,26 @@ make -C main
 fastchess \
   -engine proto=uci cmd=engine name=dev \
   -engine proto=uci cmd=main/engine name=main \
+=======
+current=$(git symbolic-ref --short HEAD)
+exe=main
+
+git checkout dev
+git pull
+make
+mv "$exe" dev
+
+git checkout main
+git pull
+make
+mv "$exe" main
+
+git checkout "$current"
+
+fastchess \
+  -engine proto=uci cmd=dev name=dev \
+  -engine proto=uci cmd=main name=main \
+>>>>>>> fdf61bc (Update sprt script to compare git branches)
   -each tc=inf/8+0.08 -openings file=books/UHO_Lichess_4852_v1.epd format=epd order=random \
   -games 2 -rounds 2500 -repeat 2 -maxmoves 200 \
   -sprt elo0=0 elo1=10 alpha=0.05 beta=0.1 -concurrency 12 -ratinginterval 10
