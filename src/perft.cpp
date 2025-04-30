2a89c548920f2e6a66bd81d15c1c0470187bfc01
Matei Hriscu <matei.hriscu@proton.me>
2025-04-30 12:53:03 +0300

    Accomodate inferior OS users

--- src/perft.cpp
+++ src/perft.cpp
@@ -1,14 +1,14 @@
 #include "perft.hpp"
 
 void splitperft(const Board &board, int depth) {
-  long total = 0;
+  int64_t total = 0;
 
   for (Move m : board.pseudolegal_moves()) {
     Board copy = board;
     copy.make_move(m);
 
     if (copy.is_legal()) {
-      long current = perft(copy, depth - 1);
+      int64_t current = perft(copy, depth - 1);
 
       std::println("{} - {}", m.uci(), current);
       total += current;
